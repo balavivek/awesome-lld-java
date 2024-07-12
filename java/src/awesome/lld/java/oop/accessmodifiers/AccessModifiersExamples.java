296d09e846fd0e976ac6b1a2e67bf8adf0750594
Balavivek <balavivekcs@gmail.com>
2024-07-12 19:34:21 +0530

    Initial code

---
+++ java/src/awesome/lld/java/oop/accessmodifiers/AccessModifiersExamples.java
@@ -0,0 +1,8 @@
+package awesome.lld.java.oop.accessmodifiers;
+
+public class AccessModifiersExamples {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
